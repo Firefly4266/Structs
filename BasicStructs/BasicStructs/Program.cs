f7453343e560772e610e25021ffbaf112459aff4
Firefly4266 <gaines.johnp@gmail.com>
2017-09-19 22:53:04 -0700

    Problem 2 completed.

--- BasicStructs/BasicStructs/Program.cs
+++ BasicStructs/BasicStructs/Program.cs
@@ -8,8 +8,22 @@ namespace BasicStructs
 {
     class Program
     {
+        public struct aNewStruct
+        {
+            public int one;
+            public void UpdateStruct(aNewStruct value)
+            {
+                value.one = 2;
+                Console.WriteLine($"\n The new value is {value.one}.\n");
+            }
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("\n Greetings user, welcome to our program.\n");
+            aNewStruct num = new aNewStruct() {one = 1 };
+            Console.WriteLine($"\n The present value of one is {num.one}\n");
+            num.UpdateStruct(num);
+            Console.WriteLine($"\n The present value of one is {num.one}\n");
         }
     }
 }
