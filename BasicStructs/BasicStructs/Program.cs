ce79f3c66036b31f8eeea13aa3373d22fb07a02d
Firefly4266 <gaines.johnp@gmail.com>
2017-09-19 22:10:53 -0700

    Problem 30 completed

--- BasicStructs/BasicStructs/Program.cs
+++ BasicStructs/BasicStructs/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace BasicStructs
 {
+    public struct aNewStruct
+    {
+        public int x;
+        public int y;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n Greetings user, welcome to our program.\n");
+            aNewStruct newStruct = new aNewStruct(){x = 6, y = 5};
+            aNewStruct swap = newStruct;
+            swap.x = 5;
+            swap.y = 6;
+            Console.WriteLine($"\n newStruct values: x = {newStruct.x}, y = {newStruct.y}\n");
+            Console.WriteLine($"\n swap values: x = {swap.x}, y = {swap.y}\n");
         }
     }
 }
