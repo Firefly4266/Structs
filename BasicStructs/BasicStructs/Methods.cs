36a9f84bb7eb0b861f9bb75c37c8bd5ee1ddaa98
Firefly4266 <gaines.johnp@gmail.com>
2017-09-23 21:03:15 -0700

    Problem 32 completed.

---
+++ BasicStructs/BasicStructs/Methods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicStructs
+{
+    class Methods
+    {
+        public static void UpdateStruct(int myNum)
+        {
+            myNum++;
+            Console.WriteLine($"\n The value of myNum from the Update method is {myNum}\n");
+        }
+        public static void UpdateClass(int num)
+        {
+            num++;
+            Console.WriteLine($"\n The value of classNum from the Update method is {num}\n");
+        }
+    }
+}
--- BasicStructs/BasicStructs/Program.cs
+++ BasicStructs/BasicStructs/Program.cs
@@ -13,12 +13,17 @@ namespace BasicStructs
     }
     class Program
     {
-        public void UpdateStruct()
-        {
-
-        }
         static void Main(string[] args)
         {
+            Console.WriteLine("\n Greetings user, welcome to my program.");
+            addStruct sampleStruct = new addStruct {myNum = 10};
+            addClass sampleClass = new addClass {classNum = 100};
+            Console.WriteLine($"\n The value of myNum from initialization is {sampleStruct.myNum}\n");
+            Methods.UpdateStruct(sampleStruct.myNum);
+            Console.WriteLine($"\n The current value of mynum is {sampleStruct.myNum}");
+            Console.WriteLine($"\n The value of classNum from initialization is {sampleClass.classNum}\n");
+            Methods.UpdateClass(sampleClass.classNum);
+            Console.WriteLine($"\n The current value of classNum is {sampleClass.classNum}\n");
         }
     }
 }
